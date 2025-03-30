38b53f55e9d43b10e56bdf05e004f1a6ba05f2b8
Andrey Skorokhodov <skoran.andrey@gmail.com>
2025-03-30 03:15:35 +0300

    Setup for testing offline features.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "tdclient.h"
 
+#include "tgfs/tgfs.h"
+
 /* Api_id and api_hash should be passed as program args */
 
 /// TODO: Implement multithread process_update
 
 int main(int argc, char *argv[]) {
+    return make_new_tgfs(argc, argv, nullptr);
     TdClass td_client(std::stoi(argv[1]), argv[2]);
     td_client.Loop();
 }
