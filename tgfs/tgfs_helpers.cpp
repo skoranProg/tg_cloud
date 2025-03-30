b6139b54337347768178f98bce31fec72875cc70
Andrey Skorokhodov <skoran.andrey@gmail.com>
2025-03-30 03:10:45 +0300

    Bugfix.

--- tgfs/tgfs_helpers.cpp
+++ tgfs/tgfs_helpers.cpp
@@ -6,7 +6,7 @@ std::mt19937_64 rnd;
 
 fuse_ino_t get_new_ino(tgfs_data &context) {
   fuse_ino_t res = static_cast<fuse_ino_t>(rnd());
-  while (res == 0 || context.lookup_msg(res) == 0) {
+  while (res == 0 || context.lookup_msg(res) != 0) {
     ++res;
   }
   return res;
