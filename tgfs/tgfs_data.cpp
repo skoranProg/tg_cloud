aeb7d4008cc748ad447b8b026db7e164f4ad169b
Andrey Skorokhodov <skoran.andrey@gmail.com>
2025-08-23 23:14:44 +0300

    Rework update and upload.
    Also new inode methods: update_data() and upload_data()

--- tgfs/tgfs_data.cpp
+++ tgfs/tgfs_data.cpp
@@ -1,14 +1,21 @@
 #include "tgfs_data.h"
-#include "tgfs_helpers.h"
-#include <format>
+
 #include <sys/mman.h>
+
+#include <format>
+
+#include "tgfs_helpers.h"
 
 tgfs_data::tgfs_data(bool debug, double timeout, int root_fd,
                      size_t max_filesize, tgfs_net_api *api)
-    : api_{api}, timeout_{timeout}, root_fd_{root_fd},
+    : api_{api},
+      timeout_{timeout},
+      root_fd_{root_fd},
       root_path_{std::format("/proc/self/fd/{}/", root_fd)},
       table_path_{std::format("{}message_table", root_path_)},
-      max_filesize_{max_filesize}, debug_{debug}, inodes_{},
+      max_filesize_{max_filesize},
+      debug_{debug},
+      inodes_{},
       messages_{table_path_} {
     tgfs_dir *root = new tgfs_dir(FUSE_ROOT_ID, FUSE_ROOT_ID);
     inodes_.emplace(FUSE_ROOT_ID, reinterpret_cast<tgfs_inode *>(root));
@@ -34,13 +41,21 @@ tgfs_data *tgfs_data::tgfs_ptr(fuse_req_t req) {
     return reinterpret_cast<tgfs_data *>(fuse_req_userdata(req));
 }
 
-bool tgfs_data::is_debug() const { return debug_; }
+bool tgfs_data::is_debug() const {
+    return debug_;
+}
 
-double tgfs_data::get_timeout() const { return timeout_; }
+double tgfs_data::get_timeout() const {
+    return timeout_;
+}
 
-int tgfs_data::get_root_fd() const { return root_fd_; }
+int tgfs_data::get_root_fd() const {
+    return root_fd_;
+}
 
-size_t tgfs_data::get_max_filesize() const { return max_filesize_; }
+size_t tgfs_data::get_max_filesize() const {
+    return max_filesize_;
+}
 
 uint64_t tgfs_data::lookup_msg(fuse_ino_t ino) {
     update_table();
@@ -75,7 +90,9 @@ int tgfs_data::upload(fuse_ino_t ino) {
     if (msg == 0) {
         return 1;
     }
-    uint64_t new_msg = api_->upload(std::format("{}{}", root_path_, ino));
+    tgfs_inode *ino_obj = lookup_inode(ino);
+    ino_obj->upload_data(api_, 0, root_path_);
+    uint64_t new_msg = api_->upload(std::format("{}{}/inode", root_path_, ino));
     api_->remove(msg);
     messages_.set(ino, new_msg);
     api_->upload_table(table_path_);
@@ -99,7 +116,8 @@ int tgfs_data::update(fuse_ino_t ino) {
         munmap(inodes_[ino], sizeof(tgfs_inode));
         inodes_.erase(ino);
     }
-    api_->download(msg, std::format("{}{}", root_path_, ino));
+    api_->download(msg, std::format("{}{}/inode", root_path_, ino));
     inodes_[ino] = map_inode(*this, ino);
+    inodes_[ino]->update_data(api_, 0, root_path_);
     return 0;
 }
--- tgfs/tgfs_inode.h
+++ tgfs/tgfs_inode.h
@@ -9,6 +9,7 @@
 #include <string>
 #include <vector>
 
+#include "tgfs.h"
 #include "tgfs_fuse_dependencies.h"
 
 class tgfs_inode {
@@ -16,12 +17,13 @@ class tgfs_inode {
     struct stat attr;
     uint64_t version;
 
-    std::vector<uint64_t> get_data_version();
-    void set_data_version(int n, uint64_t ver);
+    int update_data(tgfs_net_api *api, int n, const std::string &root_path);
+    int upload_data(tgfs_net_api *api, int n, const std::string &root_path);
 
     tgfs_inode(struct stat attr, uint64_t version);
 
  private:
+    uint64_t data_msg_;
     uint64_t data_version_;
 };
 
