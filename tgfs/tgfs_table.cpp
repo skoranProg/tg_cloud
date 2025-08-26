c9fd746ec6d71a25a79e12c0a7cccacf79fea572
Andrey Skorokhodov <skoran.andrey@gmail.com>
2025-08-27 00:40:36 +0300

    Make sync() normal and return mmap.

--- tgfs/tgfs_table.cpp
+++ tgfs/tgfs_table.cpp
@@ -4,9 +4,10 @@
 
 tgfs_db::tgfs_db(const std::string &path) {
     int err = sqlite3_open(path.c_str(), &table_);
-    // sqlite3_exec(table_, "PRAGMA mmap_size=268435456;", nullptr, nullptr,
-    //              nullptr);
-    sqlite3_exec(table_, "PRAGMA synchronous=FULL;", nullptr, nullptr, nullptr);
+    sqlite3_exec(table_, "PRAGMA mmap_size=268435456;", nullptr, nullptr,
+                 nullptr);
+    // sqlite3_exec(table_, "PRAGMA synchronous=FULL;", nullptr, nullptr,
+    // nullptr);
     std::cerr << "DB open() !!! " << err << "  " << table_ << std::endl;
 }
 
@@ -17,11 +18,13 @@ tgfs_db::~tgfs_db() {
 }
 
 void tgfs_db::sync() {
-    int err0 =
-        sqlite3_wal_checkpoint_v2(table_, "main", SQLITE_CHECKPOINT_FULL, 0, 0);
+    // int err0 =
+    //     sqlite3_wal_checkpoint_v2(table_, "main", SQLITE_CHECKPOINT_FULL, 0,
+    //     0);
     int err1 = sqlite3_db_cacheflush(table_);
-    int err2 =
-        sqlite3_file_control(table_, nullptr, SQLITE_FCNTL_SYNC, nullptr);
-    std::cerr << "DB sync() !!!" << "  " << err0 << "  " << err1 << "  " << err2
+    // int err2 =
+    //     sqlite3_file_control(table_, nullptr, SQLITE_FCNTL_SYNC, nullptr);
+    std::cerr << "DB sync() !!!" /*<< "  " << err0 */ << "  "
+              << err1 /*<< "  " << err2*/
               << std::endl;
 }
