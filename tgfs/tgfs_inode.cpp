727ea77597960e6701d06a5bdf2fe6ae30bf18c9
Andrey Skorokhodov <skoran.andrey@gmail.com>
2025-09-08 20:10:16 +0300

    Adjust size of allocated memory for new inode file

--- tgfs/tgfs_inode.cpp
+++ tgfs/tgfs_inode.cpp
@@ -10,7 +10,7 @@ tgfs_inode::tgfs_inode(fuse_ino_t ino, const std::string &root_path)
     : attr{nullptr}, nlookup{0}, version{0}, data_version_{0} {
     int fd = open(std::format("{}/{}/inode", root_path, ino).c_str(),
                   O_CREAT | O_RDWR, S_IFREG | 0666);
-    posix_fallocate(fd, 0, sizeof(struct stat));
+    posix_fallocate(fd, 0, sizeof(persistent_data));
     attr = reinterpret_cast<persistent_data *>(
         mmap(NULL, sizeof(persistent_data), PROT_READ | PROT_WRITE, MAP_SHARED,
              fd, 0));
